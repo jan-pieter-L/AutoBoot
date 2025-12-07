226051a0858ec37f05d882992d39dacc6bbc7d02
Jan-Pieter Laleman <janpieter.laleman@gmail.com>
2025-12-08 00:45:14 +0100

    multiple improvements

--- install_update_repo_service.sh
+++ install_update_repo_service.sh
@@ -1,11 +1,18 @@
-#!/bin/bash
-set -e
+cd /home/jacob # make sure we are in home directory
+rm -rf AutoBoot # remove old repo if it exists
+git clone https://github.com/jan-pieter-L/AutoBoot.git # Get all scripts from repo.
 
-# Install the update script
-cp /boot/update_repo_on_boot.sh /home/jacob/update_repo_on_boot.sh
-chmod +x /home/jacob/update_repo_on_boot.sh
-chown jacob:jacob /home/jacob/update_repo_on_boot.sh
+sudo mv /home/jacob/AutoBoot/update_repo.service /etc/systemd/system/update_repo.service #move service file to systemd folder
+sudo chmod 644 /etc/systemd/system/update_repo.service # set permissions
+sudo chown jacob:jacob /home/jacob/update_repo.log # set log file ownership
+sudo systemctl daemon-reload # reload systemd to recognize new service
 
-# Install the systemd service
-cp /boot/update-repo.service /etc/systemd/system/update-repo.service
-systemctl enable update-repo.service
+
+cd /home/jacob/AutoBoot
+cp /home/jacob/AutoBoot/update_repo.sh /home/jacob/update_repo.sh # copy the update script to home directory
+chmod +x /home/jacob/update_repo.sh # make script executable
+
+rm -rf AutoBoot # remove the cloned repo
+
+sudo systemctl restart update_repo.service # restart the service to test it
+systemctl status update_repo.service # check status
